378fe3e2cc0edcacf2b80626ed0f6cc691a2d539
Christian Danowski <christian.danowski@hs-bochum.de>
2016-07-23 16:40:56 +0200

    add BEAN definitions for dependency injection

--- src/main/java/controller/WebSocketConfig.java
+++ src/main/java/controller/WebSocketConfig.java
@@ -1,16 +1,22 @@
 package controller;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 
+import mediator_wrapper.mediation.impl.IvisMediator;
+import mediator_wrapper.wrapper.impl.CsvWrapper;
+import mediator_wrapper.wrapper.impl.XmlWrapper;
 import util.UrlConstants;
 
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
+
+	private String pathToWrapperMappingFile = "data/config/GlobalSchemaToWrapperMapping.xml";
 
 	@Override
 	public void configureMessageBroker(MessageBrokerRegistry config) {
@@ -50,6 +56,46 @@ public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
 		 * sources
 		 */
 		registry.addEndpoint(UrlConstants.RUNTIME_MODIFICATION_ENDPOINT).withSockJS();
+	}
+
+	/**
+	 * add spring beans for dependency injection
+	 */
+
+	/**
+	 * configure mediator instance
+	 * 
+	 * @return
+	 */
+	@Bean
+	public IvisMediator initializeMediator() {
+		return new IvisMediator(this.pathToWrapperMappingFile);
+	}
+
+	/**
+	 * configure xml wrapper instance
+	 * 
+	 * @return
+	 */
+	@Bean
+	public XmlWrapper initializeXmlWrapper() {
+		String fileLocation = "data/data_sources/products.xml";
+		String localSchemaMappingLocation = "data/config/XmlBookstoreMapping.xml";
+
+		return new XmlWrapper(fileLocation, localSchemaMappingLocation);
+	}
+	
+	/**
+	 * configure csv wrapper instance
+	 * 
+	 * @return
+	 */
+	@Bean
+	public CsvWrapper initializeCsvWrapper() {
+		String fileLocation = "data/data_sources/products.csv";
+		String localSchemaMappingLocation = "data/config/CsvBookstoreMapping.xml";
+
+		return new CsvWrapper(fileLocation, localSchemaMappingLocation);
 	}
 
 }
