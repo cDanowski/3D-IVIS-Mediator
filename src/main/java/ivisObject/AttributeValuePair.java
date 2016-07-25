9598d1bd67a217521515ea118e9b02036cf0c6f1
Christian Danowski <christian.danowski@hs-bochum.de>
2016-07-25 17:07:20 +0200

    add IvisObject as Java representation of retrieved objects

---
+++ src/main/java/ivisObject/AttributeValuePair.java
@@ -0,0 +1,38 @@
+package ivisObject;
+
+/**
+ * A generic class to create a pair of attribute name and value
+ * @author Christian Danowski
+ *
+ */
+public class AttributeValuePair {
+	
+	private String name;
+	
+	private Object value;
+
+	public AttributeValuePair(String name, Object value) {
+		super();
+		this.name = name;
+		this.value = value;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Object getValue() {
+		return value;
+	}
+
+	public void setValue(Object value) {
+		this.value = value;
+	}
+	
+	
+
+}
---
+++ src/main/java/ivisObject/IvisObject.java
@@ -0,0 +1,15 @@
+package ivisObject;
+
+import java.util.List;
+
+/**
+ * Java representation of generic IVIS objects retrieved by wrappers.
+ * 
+ * @author Christian Danowski
+ *
+ */
+public class IvisObject {
+
+	List<AttributeValuePair> attributeValuePairs;
+	
+}
