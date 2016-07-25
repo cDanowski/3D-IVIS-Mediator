f73be6fa986543af17ff17cb90f67f4ab7740c9c
Christian Danowski <christian.danowski@hs-bochum.de>
2016-07-25 22:08:15 +0200

    add a name property

--- src/main/java/ivisObject/IvisObject.java
+++ src/main/java/ivisObject/IvisObject.java
@@ -11,5 +11,20 @@ import java.util.List;
 public class IvisObject {
 
 	List<AttributeValuePair> attributeValuePairs;
-	
+	private String elementName;
+
+	public IvisObject(String elementName, List<AttributeValuePair> attributeValuePairs) {
+		super();
+		this.elementName = elementName;
+		this.attributeValuePairs = attributeValuePairs;
+	}
+
+	public List<AttributeValuePair> getAttributeValuePairs() {
+		return attributeValuePairs;
+	}
+
+	public String getElementName() {
+		return elementName;
+	}
+
 }
