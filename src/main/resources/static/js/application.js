3587e09b05bdd47b52153dd5c9717c4f11027b02
Christian Danowski <christian.danowski@hs-bochum.de>
2016-08-08 14:49:19 +0200

    provide simple filter mechanisms for author and price

--- src/main/resources/static/js/application.js
+++ src/main/resources/static/js/application.js
@@ -3,14 +3,18 @@ var SERVER_SIDE_VISUALIZATION_ENDPOINT = '/initial/visualize';
 var RUNTIME_ADDITIONAL_DATA_ENDPOINT = '/runtime/visualizeAdditionalData';
 var RUNTIME_MODIFY_ENDPOINT = '/runtime/modify';
 
-//STOMP ENDPOINTS
-var STOMP_SERVER_SIDE_IVIS_ENDPOINT = '/user/queue' + SERVER_SIDE_VISUALIZATION_ENDPOINT;
-var STOMP_RUNTIME_ADDITIONAL_DATA_ENDPOINT = '/user/queue' + RUNTIME_ADDITIONAL_DATA_ENDPOINT;
+// STOMP ENDPOINTS
+var STOMP_SERVER_SIDE_IVIS_ENDPOINT = '/user/queue'
+		+ SERVER_SIDE_VISUALIZATION_ENDPOINT;
+var STOMP_RUNTIME_ADDITIONAL_DATA_ENDPOINT = '/user/queue'
+		+ RUNTIME_ADDITIONAL_DATA_ENDPOINT;
 var STOMP_SYNCHRONIZE_ENDPOINT = '/topic/synchronize';
 
-//send message endpoints with prefix
-var SEND_SERVER_SIDE_VISUALIZATION_ENDPOINT = '/ivisApp' + SERVER_SIDE_VISUALIZATION_ENDPOINT;
-var SEND_RUNTIME_ADDITIONAL_DATA_ENDPOINT = '/ivisApp' + RUNTIME_ADDITIONAL_DATA_ENDPOINT;
+// send message endpoints with prefix
+var SEND_SERVER_SIDE_VISUALIZATION_ENDPOINT = '/ivisApp'
+		+ SERVER_SIDE_VISUALIZATION_ENDPOINT;
+var SEND_RUNTIME_ADDITIONAL_DATA_ENDPOINT = '/ivisApp'
+		+ RUNTIME_ADDITIONAL_DATA_ENDPOINT;
 var SEND_RUNTIME_MODIFY_ENDPOINT = '/ivisApp' + RUNTIME_MODIFY_ENDPOINT;
 
 // stomp client variable that holds the connection to server
@@ -28,17 +32,18 @@ function connect() {
 		console.log('Connected: ' + frame);
 
 		// subscriptions to STOMP endpoints
-		
-		// initial scene
-		stompClient.subscribe(STOMP_SERVER_SIDE_IVIS_ENDPOINT, function(object) {
 
-			var returnedQueryResult = JSON.parse(object.body);
-			
-			var x3domSceneString = returnedQueryResult.responseScene;
-			
-			integrateSceneIntoDOM(x3domSceneString);
-		});
-		
+		// initial scene
+		stompClient.subscribe(STOMP_SERVER_SIDE_IVIS_ENDPOINT,
+				function(object) {
+
+					var returnedQueryResult = JSON.parse(object.body);
+
+					var x3domSceneString = returnedQueryResult.responseScene;
+
+					integrateSceneIntoDOM(x3domSceneString);
+				});
+
 		// synchronization updates
 		stompClient.subscribe(STOMP_SYNCHRONIZE_ENDPOINT, function(object) {
 			// TODO
@@ -61,20 +66,87 @@ function applyUpdate(object) {
 function visualizeBookStocks() {
 	// create and send request to fetch initial scene!
 
-	var serverSideVisualizationMessage = {};
-	
-	serverSideVisualizationMessage.applicationTemplateIdentifier = "bookstoreApplicationTemplate";
-	
-	serverSideVisualizationMessage.query = {};
-	serverSideVisualizationMessage.query.selector = "bookstore/book";
-	serverSideVisualizationMessage.query.filters = undefined;
-	serverSideVisualizationMessage.query.filterStrategy = "AND";
+	var serverSideVisualizationMessage = createServerSideVisualizationRequest();
 
-	stompClient.send(SEND_SERVER_SIDE_VISUALIZATION_ENDPOINT, {}, JSON.stringify(serverSideVisualizationMessage));
+	stompClient.send(SEND_SERVER_SIDE_VISUALIZATION_ENDPOINT, {}, JSON
+			.stringify(serverSideVisualizationMessage));
 }
 
-function integrateSceneIntoDOM(x3domSceneString){
+function createServerSideVisualizationRequest() {
+
+	var serverSideVisualizationMessage = {};
+
+	serverSideVisualizationMessage.applicationTemplateIdentifier = "bookstoreApplicationTemplate";
+
+	serverSideVisualizationMessage.query = {};
+	serverSideVisualizationMessage.query.selector = "bookstore/book";
+	serverSideVisualizationMessage = configureFilters(serverSideVisualizationMessage);
 	
+	return serverSideVisualizationMessage;
+}
+
+function configureFilters(serverSideVisualizationMessage) {
+	if ($("#filter_enableCheckbox").is(':checked')) {
+		// checked
+
+		var authorFilterValue = $("#filter_author_value").val() || undefined;
+		var authorFilterComparisonMethod = $(
+		"#filter_author_comparison option:selected").text();
+		var priceFilterValue = $("#filter_price_value").val() || undefined;
+		var priceFilterComparisonMethod = $(
+				"#filter_price_comparison option:selected").text();
+
+		serverSideVisualizationMessage.query.filters = [];
+
+		if (authorFilterValue != undefined && authorFilterValue != "") {
+			/*
+			 * a filter has the following properties:
+			 * 
+			 * private String selector;
+			 * 
+			 * private Object filterValue;
+			 * 
+			 * private FilterType filterType;
+			 */
+			var authorFilter = {};
+			authorFilter.selector = "bookstore/book/author";
+			authorFilter.filterValue = authorFilterValue;
+			authorFilter.filterType = authorFilterComparisonMethod;
+			
+			serverSideVisualizationMessage.query.filters.push(authorFilter);
+		}
+		
+		if (priceFilterValue != undefined && priceFilterValue != "") {
+			/*
+			 * a filter has the following properties:
+			 * 
+			 * private String selector;
+			 * 
+			 * private Object filterValue;
+			 * 
+			 * private FilterType filterType;
+			 */
+			var priceFilter = {};
+			priceFilter.selector = "bookstore/book/price";
+			priceFilter.filterValue = priceFilterValue;
+			priceFilter.filterType = priceFilterComparisonMethod;
+			
+			serverSideVisualizationMessage.query.filters.push(priceFilter);
+		}
+
+		serverSideVisualizationMessage.query.filterStrategy = $(
+				"#filter_strategy option:selected").text();
+	} else {
+		// unchecked
+		serverSideVisualizationMessage.query.filters = undefined;
+		serverSideVisualizationMessage.query.filterStrategy = "AND";
+	}
+
+	return serverSideVisualizationMessage;
+}
+
+function integrateSceneIntoDOM(x3domSceneString) {
+
 	/*
 	 * identify "scene" div (with id "scene") and clear its content
 	 * 
@@ -82,13 +154,23 @@ function integrateSceneIntoDOM(x3domSceneString){
 	 */
 	$("#scene").empty();
 	$("#scene").append(x3domSceneString);
-	
+
 	// trigger reload to parse new x3d scene
 	x3dom.reload();
-	
+
 	// zoom/fit to all elements of the scene
 	var sceneElement = $('x3d').get(0);
 	sceneElement.runtime.showAll();
-	
+
 	console.log("should see new scene");
+}
+
+function onEnableFiltersChange() {
+	if ($("#filter_enableCheckbox").is(':checked')) {
+		// checked
+		$('.form-control').prop("disabled", false);
+	} else {
+		// unchecked
+		$('.form-control').prop("disabled", true);
+	}
 }
